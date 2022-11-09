523a76e913e421824ff69eee7a30317d7d1b49a9
Eamon <eamon0989@gmail.com>
2022-11-09 07:26:08 +0100

    test: verify hash of generated pdf

--- packages/node-red-contrib-generate-pdf-s1seven/test/generate-pdf-node.spec.js
+++ packages/node-red-contrib-generate-pdf-s1seven/test/generate-pdf-node.spec.js
@@ -1,10 +1,10 @@
 const helper = require("node-red-node-test-helper");
 const generatePdfNode = require("../generate-pdf-node.js");
 const certificate = require("../fixtures/valid_certificate_1.json");
-// const { readFileSync } = require("fs");
-// const { join } = require("path");
-
-// const expectedPdfBuffer = readFileSync(join(__dirname, "../fixtures/valid_certificate_1.pdf"));
+const { readFileSync } = require("fs");
+const { fromBuffer } = require("pdf2pic");
+const { join } = require("path");
+const { createHash } = require("crypto");
 
 helper.init(require.resolve("node-red"));
 
@@ -32,6 +32,13 @@ describe("generate pdf Node", function () {
 	});
 
 	it("should return a buffer when passed a certificate", function (done) {
+		const expectedPDFBuffer = readFileSync(join(__dirname, "../fixtures/valid_certificate_1.pdf"));
+		const options = {
+			density: 100,
+			width: 600,
+			height: 600,
+		};
+
 		const flow = [
 			{ id: "n1", type: "generate pdf", name: "generate pdf", wires: [["n2"]] },
 			{ id: "n2", type: "helper" },
@@ -39,12 +46,16 @@ describe("generate pdf Node", function () {
 		helper.load(generatePdfNode, flow, function () {
 			const n2 = helper.getNode("n2");
 			const n1 = helper.getNode("n1");
-			n2.on("input", function (msg) {
+			n2.on("input", async function (msg) {
 				try {
 					const { payload } = msg;
+					const expectedPDF = await fromBuffer(expectedPDFBuffer, options)(1, true);
+					const result = await fromBuffer(payload, options)(1, true);
+					const expectedHash = createHash("sha256").update(expectedPDF.base64).digest("hex");
+					const resultHash = createHash("sha256").update(result.base64).digest("hex");
 					expect(payload instanceof Buffer).toEqual(true);
-					// TODO: figure out why the buffer is slightly different
-					// expect(payload).toEqual(expectedPdfBuffer);
+					expect(resultHash).toEqual(expectedHash);
+
 					done();
 				} catch (err) {
 					done(err);
