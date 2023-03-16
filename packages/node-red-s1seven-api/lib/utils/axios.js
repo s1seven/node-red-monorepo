aec1f90dc88bc69f3d4479afd403e7360df1500c
Edouard <ed@getlarge.eu>
2023-03-16 16:25:35 +0100

    fix(node-red-s1seven-api): refine axios instance creation

--- packages/node-red-s1seven-api/lib/utils/axios.js
+++ packages/node-red-s1seven-api/lib/utils/axios.js
@@ -1,24 +1,36 @@
-const { Axios } = require('axios');
-const { getAccessToken, getApiUrl, getCurrentCompanyId } = require('./getters');
-
+const axios = require('axios');
+const { hostname } = require('node:os');
+const {
+  getAccessToken,
+  getApiUrl,
+  getApiVersion,
+  getCurrentCompanyId,
+} = require('./getters');
+const { pkgVersion } = require('../package.json');
 /**
  *
+ * @param {import('node-red').NodeContext['global']} globalContext
  * @returns {import('axios').AxiosInstance}
  */
-function axios() {
+function createAxiosInstance(globalContext) {
   const version = getApiVersion();
-  const accessToken = getAccessToken();
-  const companyId = getCurrentCompanyId();
+  const accessToken = getAccessToken(globalContext);
+  const companyId = getCurrentCompanyId(globalContext);
 
-  return new Axios({
+  return axios.create({
     baseURL: getApiUrl(),
     headers: {
       'Content-Type': 'application/json',
       ...(accessToken ? { Authentication: `bearer ${accessToken}` } : {}),
       ...(companyId ? { company: companyId } : {}),
       'x-version': `${version}`,
+      'user-agent': `node-red-s1seven-api/${pkgVersion}/${hostname}`,
     },
+    validateStatus: (status) => status >= 200 && status < 300,
+    responseType: 'json',
   });
 }
 
-module.exports = axios;
+module.exports = {
+  createAxiosInstance,
+};
