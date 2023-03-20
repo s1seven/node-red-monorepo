96a3d86b381096f7adce793b49495cea21c50783
Eamon <eamon0989@gmail.com>
2023-03-20 16:07:57 +0100

    feat: add validate endpoint node

---
+++ packages/node-red-s1seven-api/lib/certificates/validate-certificate.js
@@ -0,0 +1,62 @@
+'use strict';
+
+require('dotenv').config({
+  path: require('node:path').resolve(__dirname, '../../.env'),
+});
+// we need to setup the container before requiring the node to allow to override the dependencies in the tests
+require('../utils/container').setupContainer();
+
+/**
+ * @type {RED_JS}
+ */
+module.exports = function (RED) {
+  const { container } = require('../utils/container');
+  const validateCertificate = require('../utils/validateCertificate');
+
+  /** @type {import('../utils/async-local-storage')} */
+  const asyncLocalStorage = container.resolve('asyncLocalStorage');
+  /** @type {import('../utils/getters')} */
+  const getters = container.resolve('getters');
+  /** @type {import('../utils/axios-helpers')} */
+  const axiosHelpers = container.resolve('axiosHelpers');
+
+  function validateCertificateNode(config) {
+    /** @type NodeRedNode */
+    const node = this;
+    RED.nodes.createNode(node, config);
+    const globalContext = node.context().global;
+    const apiConfig = RED.nodes.getNode(config.apiConfig);
+
+    node.on('input', async (msg, send, cb) => {
+      function done(err) {
+        asyncLocalStorage.exit(cb, err);
+      }
+      asyncLocalStorage.init({ apiConfig, globalContext, msg });
+
+      let certificate = msg.payload || globalContext.get('certificate');
+      const accessToken = getters.getAccessToken();
+      try {
+        certificate = validateCertificate(certificate);
+      } catch (error) {
+        node.error(RED._('validate.errors.validCertificate'));
+        done();
+        return;
+      }
+      if (!accessToken) {
+        node.warn(RED._('validate.errors.accessToken'));
+        done();
+        return;
+      }
+
+      const axios = axiosHelpers.createAxiosInstance();
+      const { success, data } = await axiosHelpers.requestHandler(
+        axios.post('/certificates/validate', certificate),
+        send
+      );
+
+      !success && node.error(data);
+      done();
+    });
+  }
+  RED.nodes.registerType('validate certificate', validateCertificateNode);
+};
